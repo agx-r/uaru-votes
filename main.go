cdb45a58b84b4d14c22a3c1b359def4213c5912d
Nikita Mitasov <me@ch4og.com>
2025-07-30 17:49:10 +0300

    refactor main

--- main.go
+++ main.go
@@ -53,6 +53,117 @@ func (h *errorHandler) handleError(err error, _ tb.Context) {
 	}
 }
 
+func isAdmin(userID int64, admins []tb.ChatMember) bool {
+	for _, admin := range admins {
+		if admin.User.ID == userID {
+			return true
+		}
+	}
+
+	return false
+}
+
+func botCanMute(botID int64, admins []tb.ChatMember) bool {
+	for _, admin := range admins {
+		if admin.User.ID == botID && admin.CanRestrictMembers {
+			return true
+		}
+	}
+
+	return false
+}
+
+func handlePollResult(bot *tb.Bot, log *slog.Logger, msg *tb.Message, member *tb.ChatMember, ctx tb.Context) {
+	time.Sleep(time.Hour)
+
+	poll, err := bot.StopPoll(msg)
+	if err != nil {
+		log.Error("failed to stop the poll", errorAttr(err))
+
+		return
+	}
+
+	shouldMute := poll.Options[0].VoterCount > poll.Options[1].VoterCount
+
+	if _, err := bot.Reply(msg, map[bool]string{
+		true:  "Мут nyuuu",
+		false: "Размучен.",
+	}[shouldMute]); err != nil {
+		log.Error("failed to reply to poll", errorAttr(err))
+	}
+
+	perm := !shouldMute // value of permissions is opposite of shouldMute
+
+	member.RestrictedUntil = map[bool]int64{
+		true:  tb.Forever(), // tb.Forever() is int64
+		false: 0,
+	}[shouldMute]
+	member.CanSendMessages = perm
+	member.CanSendMedia = perm
+	member.CanSendPolls = perm
+	member.CanSendOther = perm
+	member.CanAddPreviews = perm
+
+	if err := bot.Restrict(ctx.Chat(), member); err != nil {
+		logWord := map[bool]string{true: "mute", false: "unmute"}[shouldMute]
+		msgWord := map[bool]string{true: "замутить", false: "размутить"}[shouldMute]
+
+		log.Error(fmt.Sprintf("cannot %s user", logWord), errorAttr(err))
+
+		if _, err := bot.Reply(msg, "Чота не могу "+msgWord); err != nil {
+			log.Error("can't even cry", errorAttr(err))
+		}
+	}
+}
+
+func handleVoteban(bot *tb.Bot, log *slog.Logger) func(tb.Context) error {
+	return func(ctx tb.Context) error {
+		if !ctx.Message().FromGroup() {
+			return ctx.Reply("В лс не баню сори")
+		}
+
+		if ctx.Message().ReplyTo == nil {
+			return ctx.Reply("Ответь на сообщение кого забанить")
+		}
+
+		userToBan := ctx.Message().ReplyTo.Sender
+		member, err := bot.ChatMemberOf(ctx.Chat(), userToBan)
+
+		if err != nil {
+			return fmt.Errorf("failed to get member: %w", err)
+		}
+
+		admins, err := bot.AdminsOf(ctx.Chat())
+		if err != nil {
+			return fmt.Errorf("failed to get admins: %w", err)
+		}
+
+		if isAdmin(userToBan.ID, admins) {
+			return ctx.Reply("ммм не")
+		}
+
+		if !botCanMute(bot.Me.ID, admins) {
+			return ctx.Reply("Админом меня сделай, олух")
+		}
+
+		msg, err := bot.Send(ctx.Chat(), &tb.Poll{
+			Question:  "Забанить или разбанить?",
+			Anonymous: false,
+			Options: []tb.PollOption{
+				{Text: "Мут"},
+				{Text: "Размут"},
+			},
+		})
+		if err != nil {
+			return fmt.Errorf("failed to send poll: %w", err)
+		}
+
+		go handlePollResult(bot, log, msg, member, ctx)
+
+		return nil
+	}
+}
+
 func main() {
 	_ = godotenv.Load()
 
@@ -92,118 +203,14 @@ func main() {
 		Token:   token,
 		OnError: eHandler.handleError,
 	})
-
 	if err != nil {
 		log.Error("failed to initialize bot:", errorAttr(err))
 		os.Exit(1)
 	}
 
-	bot.Handle("/start", func(ctx tb.Context) error {
-		return ctx.Reply("Приве")
-	})
+	bot.Handle("/start", func(ctx tb.Context) error { return ctx.Reply("Приве") })
 
-	bot.Handle("/voteban", func(ctx tb.Context) error {
-		if !ctx.Message().FromGroup() {
-			return ctx.Reply("В лс не баню сори")
-		}
-
-		if ctx.Message().ReplyTo == nil {
-			return ctx.Reply("Ответь на сообщение кого забанить")
-		}
-
-		userToBan := ctx.Message().ReplyTo.Sender
-		member, err := bot.ChatMemberOf(ctx.Chat(), userToBan)
-
-		if err != nil {
-			return fmt.Errorf("failed to get member: %w", err)
-		}
-
-		admins, err := bot.AdminsOf(ctx.Chat())
-		if err != nil {
-			return fmt.Errorf("failed to get admins: %w", err)
-		}
-
-		amIAdmin := false
-
-		for _, admin := range admins {
-			if userToBan.ID == admin.User.ID {
-				return ctx.Reply("ммм не")
-			}
-
-			if admin.CanRestrictMembers && admin.User.ID == bot.Me.ID {
-				amIAdmin = true
-			}
-		}
-
-		if !amIAdmin {
-			return ctx.Reply("Админом меня сделай, олух")
-		}
-
-		msg, err := bot.Send(ctx.Chat(), &tb.Poll{
-			Question:  "Забанить или разбанить?",
-			Anonymous: false,
-			Options: []tb.PollOption{
-				{Text: "Мут"},
-				{Text: "Размут"},
-			},
-		})
-		if err != nil {
-			return fmt.Errorf("failed to send poll: %w", err)
-		}
-
-		go func() {
-			time.Sleep(time.Hour)
-
-			poll, err := bot.StopPoll(msg)
-			if err != nil {
-				log.Error("failed to stop the poll", errorAttr(err))
-
-				return
-			}
-
-			if poll.Options[0].VoterCount > poll.Options[1].VoterCount {
-				if _, err := bot.Reply(msg, "Мут nyuuu"); err != nil {
-					log.Error("failed to reply to poll", errorAttr(err))
-				}
-
-				member.RestrictedUntil = tb.Forever()
-				member.CanSendMessages = false
-				member.CanSendMedia = false
-				member.CanSendPolls = false
-				member.CanSendOther = false
-				member.CanAddPreviews = false
-
-				if err := bot.Restrict(ctx.Chat(), member); err != nil {
-					log.Error("cannot mute user", errorAttr(err))
-
-					if _, err := bot.Reply(msg, "Чота не могу замутить"); err != nil {
-						log.Error("can't even cry", errorAttr(err))
-					}
-				}
-			} else {
-				if _, err := bot.Reply(msg, "Размучен."); err != nil {
-					log.Error("failed to reply to poll", errorAttr(err))
-				}
-
-				member.RestrictedUntil = 0
-				member.CanSendMessages = true
-				member.CanSendMedia = true
-				member.CanSendPolls = true
-				member.CanSendOther = true
-				member.CanAddPreviews = true
-
-				if err := bot.Restrict(ctx.Chat(), member); err != nil {
-					log.Error("cannot unmute user", errorAttr(err))
-
-					if _, err := bot.Reply(msg, "Чота не могу размутить"); err != nil {
-						log.Error("can't even cry", errorAttr(err))
-					}
-				}
-			}
-		}()
-
-		return nil
-	})
+	bot.Handle("/voteban", handleVoteban(bot, log))
 
 	bot.Start()
 }
