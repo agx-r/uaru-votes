c0ccf570090f0d9cae043994c089a7b7dc193f3b
agx <agx@nyantoo.xyz>
2025-08-31 17:48:31 +0500

    feat: integrate services into bot structure

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -2,31 +2,54 @@ package bot
 
 import (
 	"log/slog"
-	"math/rand"
-	"time"
 
 	"github.com/uaru-shit/votes/internal/bot/handlers"
 	"github.com/uaru-shit/votes/internal/domain"
+	"github.com/uaru-shit/votes/internal/services"
 	tb "gopkg.in/telebot.v4"
 )
 
 type Bot struct {
-	bot    *tb.Bot
-	logger *slog.Logger
+	bot           *tb.Bot
+	logger        *slog.Logger
+	pollStorage   domain.PollStorage
+	pollMonitor   *services.PollMonitorService
+	messageFilter *services.MessageFilterService
 }
 
-func New(logger *slog.Logger, bot *tb.Bot) *Bot {
+func New(logger *slog.Logger, bot *tb.Bot, pollStorage domain.PollStorage) *Bot {
+	permissionService := services.NewPermissionService(bot, logger)
+	pollProcessor := services.NewPollProcessorService(bot, logger, permissionService)
+	pollMonitor := services.NewPollMonitorService(bot, logger, pollStorage, pollProcessor)
+	messageFilter := services.NewMessageFilterService(bot, logger)
+
 	b := &Bot{
-		bot:    bot,
-		logger: logger,
+		bot:           bot,
+		logger:        logger,
+		pollStorage:   pollStorage,
+		pollMonitor:   pollMonitor,
+		messageFilter: messageFilter,
 	}
 
+	b.setupHandlers()
+	go pollMonitor.RestoreActivePolls()
+
+	return b
+}
+
+func (b *Bot) setupHandlers() {
 	b.handle("/voteban", handlers.HandleVoteban)
 	b.handle("/vote", handlers.HandleVoteban)
 	b.handle("/ban", handlers.HandleVoteban)
 
+	b.handle("/voteunban", handlers.HandleVoteUnban)
+	b.handle("/unban", handlers.HandleVoteUnban)
+
 	b.handle("/votegif", handlers.HandleVoteGifs)
 	b.handle("/gif", handlers.HandleVoteGifs)
+
+	b.handle("/votemedia", handlers.HandleVoteMedia)
+	b.handle("/media", handlers.HandleVoteMedia)
 
 	b.bot.Handle(tb.OnText, b.handleAllMessages)
 	b.bot.Handle(tb.OnPhoto, b.handleAllMessages)
@@ -35,30 +58,10 @@ func New(logger *slog.Logger, bot *tb.Bot) *Bot {
 	b.bot.Handle(tb.OnAudio, b.handleAllMessages)
 	b.bot.Handle(tb.OnVoice, b.handleAllMessages)
 	b.bot.Handle(tb.OnSticker, b.handleAllMessages)
-
-	return b
 }
 
 func (b *Bot) handleAllMessages(tbCtx tb.Context) error {
-	msg := tbCtx.Message()
-
-	if msg.Sender.ID == 7952262321 {
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		if r.Float64() < 0.6 {
-			if err := b.bot.Delete(msg); err != nil {
-				b.logger.Error("failed to delete message",
-					slog.Int64("user_id", msg.Sender.ID),
-					slog.Int64("message_id", int64(msg.ID)),
-					slog.String("error", err.Error()))
-			} else {
-				b.logger.Info("message deleted",
-					slog.Int64("user_id", msg.Sender.ID),
-					slog.Int64("message_id", int64(msg.ID)))
-			}
-		}
-	}
-
-	return nil
+	return b.messageFilter.HandleMessage(tbCtx.Message())
 }
 
 func (b *Bot) Start() {
@@ -68,13 +71,17 @@ func (b *Bot) Start() {
 type botContext struct {
 	tb.Context
 
-	bot *tb.Bot
-
-	logger *slog.Logger
+	bot         *Bot
+	logger      *slog.Logger
+	pollStorage domain.PollStorage
 }
 
 func (ctx *botContext) BotUser() *tb.User {
-	return ctx.bot.Me
+	return ctx.bot.bot.Me
+}
+
+func (ctx *botContext) BotAPI() tb.API {
+	return ctx.bot.bot
 }
 
 func (ctx *botContext) Log() *slog.Logger {
@@ -86,7 +93,16 @@ func (ctx *botContext) WithLogger(logger *slog.Logger) domain.Context {
 		Context: ctx.Context,
 		bot:     ctx.bot,
 		logger:  logger,
+		pollStorage: ctx.pollStorage,
 	}
+}
+
+func (ctx *botContext) PollStorage() domain.PollStorage {
+	return ctx.pollStorage
+}
+
+func (ctx *botContext) StartPollMonitoring(poll *domain.ActivePoll) {
+	ctx.bot.pollMonitor.StartPollMonitoring(poll)
 }
 
 func (b *Bot) handle(endpoint any, handler func(domain.Context) error) {
@@ -97,9 +113,10 @@ func (b *Bot) handle(endpoint any, handler func(domain.Context) error) {
 		}
 
 		ctx := &botContext{
-			Context: tbCtx,
-			bot:     b.bot,
-			logger:  logger,
+			Context:     tbCtx,
+			bot:         b,
+			logger:      logger,
+			pollStorage: b.pollStorage,
 		}
 
 		return handler(ctx)
